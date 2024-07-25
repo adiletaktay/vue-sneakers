2a6eb8743690d90daab57e996957dbbd16f414b7
ADI <adiletaktay@gmail.com>
2024-07-25 14:45:56 +0500

    add drawer cart

--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -1,14 +1,20 @@
 <script setup>
 import Card from './Card.vue'
+
+const onClickAdd = () => {
+  alert('Добавить')
+}
 </script>
 
 <template>
   <div class="grid grid-cols-4 gap-5">
-    <Card />
-    <Card />
-    <Card />
-    <Card />
-    <Card />
-    <Card />
+    <Card
+      title="Мужские Кроссовки Nike Blazer Mid Suede"
+      imageUrl="/sneakers/sneakers-1.jpg"
+      :price="5000"
+      :isFavorite="false"
+      :onClickAdd="onClickAdd"
+      :isAdded="true"
+    />
   </div>
 </template>
