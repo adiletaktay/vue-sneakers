c138292765ab79fbcb5451e0c85631c7cd37c5f2
ADI <adiletaktay@gmail.com>
2024-07-26 12:18:56 +0500

    add search filter

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,35 @@
 <script setup>
 import Header from './components/Header.vue'
 import CardList from './components/CardList.vue'
-import Drawer from './components/Drawer.vue'
+// import Drawer from './components/Drawer.vue'
 </script>
 
 <template>
-  <Drawer />
+  <!-- <Drawer /> -->
   <div class="bg-white w-4/5 m-auto rounded-xl shadow-xl mt-14">
     <Header />
 
     <div class="p-10">
-      <h2 class="text-3xl font-bold mb-8">Все кроссовки</h2>
+      <div class="flex justify-between items-center">
+        <h2 class="text-3xl font-bold mb-8">Все кроссовки</h2>
+
+        <div class="flex gap-4">
+          <select class="py-2 px-3 border rounded-md outline-none">
+            <option>По названию</option>
+            <option>По цене (дешевые)</option>
+            <option>По цене (дорогие)</option>
+          </select>
+
+          <div class="relative">
+            <img class="absolute left-4 top-3" src="/search.svg" alt="search" />
+            <input
+              class="border rounded-md pl-11 py-2 pr-4 outline-none focus:border-gray-400"
+              placeholder="Поиск..."
+              type="text"
+            />
+          </div>
+        </div>
+      </div>
 
       <CardList />
     </div>
