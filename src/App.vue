5c53fdbe2a40ca7aee4dc4dee4dc3cbec36899f8
ADI <adiletaktay@gmail.com>
2024-08-01 13:34:07 +0500

    add axios

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,47 @@
 <script setup>
+import { onMounted, reactive, ref, watch } from 'vue'
+import axios from 'axios'
+
 import Header from './components/Header.vue'
 import CardList from './components/CardList.vue'
 // import Drawer from './components/Drawer.vue'
+
+const items = ref([])
+
+const filters = reactive({
+  sortBy: 'title',
+  searchQuery: ''
+})
+
+const onChangeSelect = (event) => {
+  filters.sortBy = event.target.value
+}
+
+const onChangeSearchInput = (event) => {
+  filters.searchQuery = event.target.value
+}
+
+const fetchItems = async () => {
+  try {
+    const params = {
+      sortBy: filters.sortBy
+    }
+
+    if (filters.searchQuery) {
+      params.title = `*${filters.searchQuery}*`
+    }
+
+    const { data } = await axios.get('https://2619d4cd4e08af3b.mokky.dev/items', {
+      params
+    })
+    items.value = data
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+onMounted(fetchItems)
+watch(filters, fetchItems)
 </script>
 
 <template>
@@ -14,15 +54,16 @@ import CardList from './components/CardList.vue'
         <h2 class="text-3xl font-bold mb-8">Все кроссовки</h2>
 
         <div class="flex gap-4">
-          <select class="py-2 px-3 border rounded-md outline-none">
-            <option>По названию</option>
-            <option>По цене (дешевые)</option>
-            <option>По цене (дорогие)</option>
+          <select @change="onChangeSelect" class="py-2 px-3 border rounded-md outline-none">
+            <option value="name">По названию</option>
+            <option value="price">По цене (дешевые)</option>
+            <option value="-price">По цене (дорогие)</option>
           </select>
 
           <div class="relative">
             <img class="absolute left-4 top-3" src="/search.svg" alt="search" />
             <input
+              @input="onChangeSearchInput"
               class="border rounded-md pl-11 py-2 pr-4 outline-none focus:border-gray-400"
               placeholder="Поиск..."
               type="text"
@@ -31,7 +72,9 @@ import CardList from './components/CardList.vue'
         </div>
       </div>
 
-      <CardList />
+      <div class="mt-10">
+        <CardList :items="items" />
+      </div>
     </div>
   </div>
 </template>
